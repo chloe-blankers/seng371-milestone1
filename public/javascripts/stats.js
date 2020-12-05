c8da5d3d347d125a235cddb3d63588df7b7ca980
epwr <ericpower@outlook.com>
2020-12-04 21:44:17 -0500

    Set up basic Chart.js charts on the stats page. Need the REST API calls up before I can actually implement the graphs using data.

--- public/javascripts/stats.js
+++ public/javascripts/stats.js
@@ -1,4 +1,57 @@
 
 
-let newHTML = '<div style="background: #000000; height: 100px; width: 100px;">';
-document.getElementByID('graph-1').appendChild( newHTML );
+function loadGraph( graphID, url ){
+
+    // Get Data from Server Through REST API
+    /*const response = await fetch( url );
+    const responseJSON = await response.json();
+    const whaleData = JSON.parse( responseJSON );*/
+
+    let graphHolder = document.getElementById( graphID );
+    let canvasElement = document.createElement('canvas');
+    var myChart = new Chart(canvasElement, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+    graphHolder.replaceChild( canvasElement, graphHolder.getElementsByClassName ('graph-loading')[0] );
+
+}
+
+const getWhaleData = async () => {
+
+    const response = await fetch('http://example.com/movies.json');
+    const myJson = await response.json();
+
+}
