ae78548add04b53b50dbfc6aceae205b9aeb6627
epwr <ericpower@outlook.com>
2020-12-05 23:07:53 -0500

    Added another graph (ave weight per species) and some minor css tweaks to better size the graphs on various screen dimensions.

--- public/javascripts/stats.js
+++ public/javascripts/stats.js
@@ -1,15 +1,24 @@
 
 
+
+
 function loadWhaleNumberGraph( graphID, dataJSON ){
+
+    // Get relevant graph Holder
+    let graphHolder = document.getElementById( graphID );
+
     // Parse JSON
     let data = JSON.parse( dataJSON );
     if ( data.whales[data.whales.length - 1].id != -1 ){
-        console.log("ERROR: Expected an empty array element with id == -1 in last position in JSON string (in loadWhaleFrequencyGraph();)");
+        console.log("ERROR: Expected an empty array element with id == -1 in last position in JSON string (in loadWhaleNumberGraph();)");
+        let header = graphHolder.getElementsByClassName('graph-loading')[0].getElementsByClassName('graph-loading-message')[0];
+        header.style = "color: var(--error);"
+        header.innerHTML = "Error: Unable to Process Data."
         return false;
     }
     data.whales.pop();
 
-    // Calculate Frequencies
+    // Calculate Number of Whales of Each Species
     let whaleFreq = {};
     let keys = [];
     for ( i in data.whales ){
@@ -20,8 +29,6 @@ function loadWhaleNumberGraph( graphID, dataJSON ){
             whaleFreq[data.whales[i].species] = 1;
             keys.push( data.whales[i].species );
         }
-        console.log( whaleFreq[data.whales[i].species] );
-
     }
 
     // Get data in format that ChartJS likes
@@ -30,7 +37,7 @@ function loadWhaleNumberGraph( graphID, dataJSON ){
         vals.push( whaleFreq[keys[i]] );
     }
 
-    let graphHolder = document.getElementById( graphID );
+    // Create Chart
     let canvasElement = document.createElement('canvas');
     var myChart = new Chart(canvasElement, {
         type: 'bar',
@@ -46,6 +53,7 @@ function loadWhaleNumberGraph( graphID, dataJSON ){
         },
         options: {
             responsive: true,
+            maintainAspectRatio: false,
             legend: {
                 display: false
             },
@@ -63,17 +71,88 @@ function loadWhaleNumberGraph( graphID, dataJSON ){
             }
         }
     });
-    canvasElement.style = "width: 100px;"
     graphHolder.replaceChild( canvasElement, graphHolder.getElementsByClassName('graph-loading')[0] );
 }
 
 
-function loadGraph( graphID, url ){
+function loadWhaleWeightGraph( graphID, dataJSON ){
 
+    // Get relevant graph Holder
     let graphHolder = document.getElementById( graphID );
-    let canvasElement = document.createElement('canvas');
-    //var myChart = createChart();
-    canvasElement.style = "width: 100px;"
-    graphHolder.replaceChild( canvasElement, graphHolder.getElementsByClassName('graph-loading')[0] );
 
+    // Parse JSON
+    console.log( dataJSON );
+    let data = JSON.parse( dataJSON );
+    if ( data.whales[data.whales.length - 1].id != -1 ){
+        let header = graphHolder.getElementsByClassName('graph-loading')[0].getElementsByClassName('graph-loading-message')[0];
+        header.style = "color: var(--error);"
+        header.innerHTML = "Error: Unable to Process Data."
+        return false;
+    }
+    data.whales.pop();
+
+    // Calculate Average Weights
+    let whaleDict = {};
+    for ( var whale in data.whales ){
+        whale = data.whales[whale];
+        if ( whaleDict[whale.species] ) {
+            whaleDict[whale.species] = [ (whaleDict[whale.species][0] * whaleDict[whale.species][1] +
+                               whale.weight) / (whaleDict[whale.species][1] + 1), whaleDict[whale.species][1] + 1 ];
+        }
+        else {
+            whaleDict[whale.species] = [ whale.weight, 1 ];
+        }
+    }
+
+    // Get data in format that ChartJS likes
+    let keys = [];
+    let vals = [];
+    let cusStepSize = 0;
+    for ( var key in whaleDict ){
+        keys.push(key);
+        vals.push(whaleDict[key][0]);
+        if ( whaleDict[key][0] > cusStepSize ) cusStepSize = whaleDict[key][0];
+    }
+    if (cusStepSize > 100000 ) cusStepSize = 25000;
+    else if (cusStepSize > 50000 ) cusStepSize = 10000;
+    else if (cusStepSize > 10000 ) cusStepSize = 2500;
+    else if (cusStepSize > 1000 ) cusStepSize = 250;
+    else cusStepSize = 10;
+
+    // Create Chart
+    let canvasElement = document.createElement('canvas');
+    var myChart = new Chart(canvasElement, {
+        type: 'bar',
+        data: {
+            labels: keys,
+            datasets: [{
+                label:              'Ave Weight of Whales Per Species',
+                data:               vals,
+                backgroundColor:    '#2196f3',
+                borderColor:        '#2196f3',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            legend: {
+                display: false
+            },
+            title: {
+                display: false,
+                text: 'Chart of the Average Weight of Whales of each Species.'
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        stepSize: cusStepSize
+                    }
+                }]
+            }
+        }
+    });
+    graphHolder.replaceChild( canvasElement, graphHolder.getElementsByClassName('graph-loading')[0] );
 }
+
