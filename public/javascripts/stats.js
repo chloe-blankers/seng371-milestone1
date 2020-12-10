6c025868419c2e17f94fc0a17035f90d229531c6
epwr <ericpower@outlook.com>
2020-12-10 01:34:42 -0500

    Basic set-up for observations JSON parsing.

--- public/javascripts/stats.js
+++ public/javascripts/stats.js
@@ -1,6 +1,6 @@
 
 
-async function loadGraphs( gidWhaleNum, gidWhaleWeight, gidObservationDays ){
+async function loadGraphs( gidWhaleNum, gidWhaleWeight, gidObsID ){
 
     // Use REST API to get JSON representing all the whales.
     const whaleResponse = await fetch( '/Whales/getWhales', { method: 'GET', headers: { 'Accept': 'application/txt+json'} } )
@@ -10,12 +10,84 @@ async function loadGraphs( gidWhaleNum, gidWhaleWeight, gidObservationDays ){
                 loadWhaleWeightGraph( gidWhaleWeight, data['body'] )
         } );
 
+    console.log( "TIME TO LEARN");
     // Use REST API to get JSON Representing all the observations.
-
-
-
-
+    const obsResponse = await fetch( '/Stats', { method: 'GET', headers: { 'Accept': 'application/txt+json'} } )
+            .then( (debug) => {console.log(debug); return debug;})
+            .then( (data) => data.json() )
+            .then( (data) => {
+                    loadObsGraph( gidObsID, data['body'] )
+            } );
 }
+
+function loadObsGraph( graphID, obsJSON ){
+
+    console.log( "START OF LOAD OBS GRAPH FUNCTION CALL");
+
+
+    // Get relevant graph Holder
+    let graphHolder = document.getElementById( graphID );
+
+    // Parse JSON
+    let observations = [];
+    for ( i in obsJSON ){
+        observations.push( whalesJSON[i] );
+    }
+
+    // Calculate Monthly Observations
+
+    // Get data in format that ChartJS likes
+    let keys = [ 'January, February, March, April, May, June'];
+    let data = [ 10, 15, 50, 12, 43, 39 ];
+    let cusStepSize = 10;
+    if (cusStepSize > 100000 ) cusStepSize = 25000;
+    else if (cusStepSize > 50000 ) cusStepSize = 10000;
+    else if (cusStepSize > 10000 ) cusStepSize = 2500;
+    else if (cusStepSize > 1000 ) cusStepSize = 250;
+    else cusStepSize = 10;
+
+
+
+    // Create Chart
+    let canvasElement = document.createElement('canvas');
+    var myChart = new Chart(canvasElement, {
+        type: 'line',
+        data: {
+            labels: keys,
+            datasets: [{
+                label:              'Observations per Month',
+                data:               vals,
+                backgroundColor:    '#2196f3',
+                borderColor:        '#2196f3',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            legend: {
+                display: false
+            },
+            title: {
+                display: false,
+                text: 'Chart of the Observations per month for the last 6 months.'
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        stepSize: cusStepSize
+                    }
+                }]
+            }
+        }
+    });
+    graphHolder.replaceChild( canvasElement, graphHolder.getElementsByClassName('graph-loading')[0] );
+}
+
+
+
+
 
 function loadWhaleNumberGraph( graphID, whalesJSON ){
 
