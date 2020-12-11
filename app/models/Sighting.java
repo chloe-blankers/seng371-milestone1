9f8ec0e19caac10a54c556d0702ea97ce374ffb7
LucasAntonsen <66390364+LucasAntonsen@users.noreply.github.com>
2020-12-11 14:53:13 -0800

    Touch up of Sighting

--- app/models/Sighting.java
+++ app/models/Sighting.java
@@ -1,29 +1,24 @@
 package models;
 
-import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
-
 public class Sighting {
     String whaleIDList;
     String location;
     String date;
     String time;
 
-
     /**
      *   Sighting object is different than the Observation object in that it only
-     *   stores a comma-seperated String The ID's of the Whales instead of a List of the
+     *   stores a comma-separated String, the ID's of the Whales, instead of a List of the
      *   Whales like Observation. Sighting Object works well with the Play Framework forms.
-     *   Sightings contain a comma-seperated String The ID's of the Whales in the Sighting
-     *   Ex. 5,7,2,6,3,6
+     *   Ex. 5,7,2,6,3,1
      *   The ID's of the Whales are found on the Whales page of the App.
-     *   The whaleIDList is submitted on the form on the Observation page of the app
+     *   The whaleIDList is submitted on the observation creation form on the Observation page of the app
      *
      *
-     *    @param whaleIDList  A comma-seperated String The ID's of the Whales in the Sighting
-     *                        Ex. 5,7,2,6,3,6
+     *    @param whaleIDList  A comma-separated String, the ID's of the Whales in the Sighting
+     *                        Ex. 5,7,2,6,3,1
      *                        The ID's of the Whales are found on the Whales page of the App.
-     *                        The whaleIDList is submitted on the form on the Observation page of the app
+     *                        The whaleIDList is submitted on the observation creation form on the Observation page of the app
      *    @param location     The location of the sighting
      *    @param date         The date of the sighting
      *    @param time         The time of the sighting
